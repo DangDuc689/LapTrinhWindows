b33756a314a76afbe5ce6ebff5b104bbfc40e147
DangDuc689 <folo30000@gmail.com>
2025-11-30 15:16:28 +0700

    bt3

---
+++ BaiTapTuan/BTTuan3/BTTuan3/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace BTTuan3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.GroupBox groupBox1;
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.btnAddName = new System.Windows.Forms.Button();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ádfasdfToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ádfasdfToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ádfasdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ádfasdfasfToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.detailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lstThongTin = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.ForeColor = System.Drawing.Color.Black;
+            this.lblLastName.Location = new System.Drawing.Point(33, 52);
+            this.lblLastName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(72, 16);
+            this.lblLastName.TabIndex = 0;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(this.btnAddName);
+            groupBox1.Controls.Add(this.txtPhone);
+            groupBox1.Controls.Add(this.txtFirstName);
+            groupBox1.Controls.Add(this.label6);
+            groupBox1.Controls.Add(this.label5);
+            groupBox1.Controls.Add(this.lblLastName);
+            groupBox1.Controls.Add(this.txtLastName);
+            groupBox1.Location = new System.Drawing.Point(549, 91);
+            groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            groupBox1.Size = new System.Drawing.Size(316, 363);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            // 
+            // btnAddName
+            // 
+            this.btnAddName.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAddName.Location = new System.Drawing.Point(192, 315);
+            this.btnAddName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAddName.Name = "btnAddName";
+            this.btnAddName.Size = new System.Drawing.Size(100, 28);
+            this.btnAddName.TabIndex = 1;
+            this.btnAddName.Text = "Add Name";
+            this.btnAddName.UseVisualStyleBackColor = true;
+            this.btnAddName.Click += new System.EventHandler(this.btnAddName_Click);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(37, 251);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(215, 22);
+            this.txtPhone.TabIndex = 0;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(37, 162);
+            this.txtFirstName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(215, 22);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(33, 231);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 16);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Phone";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(33, 143);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "First Name";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(37, 71);
+            this.txtLastName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(215, 22);
+            this.txtLastName.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ádfasdfToolStripMenuItem,
+            this.ádfasdfToolStripMenuItem1,
+            this.ádfasdToolStripMenuItem,
+            this.ádfasdfasfToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(878, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ádfasdfToolStripMenuItem
+            // 
+            this.ádfasdfToolStripMenuItem.Name = "ádfasdfToolStripMenuItem";
+            this.ádfasdfToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.ádfasdfToolStripMenuItem.Text = "File";
+            // 
+            // ádfasdfToolStripMenuItem1
+            // 
+            this.ádfasdfToolStripMenuItem1.Name = "ádfasdfToolStripMenuItem1";
+            this.ádfasdfToolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.ádfasdfToolStripMenuItem1.Text = "View";
+            // 
+            // ádfasdToolStripMenuItem
+            // 
+            this.ádfasdToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.formatToolStripMenuItem});
+            this.ádfasdToolStripMenuItem.Name = "ádfasdToolStripMenuItem";
+            this.ádfasdToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.ádfasdToolStripMenuItem.Text = "Format";
+            // 
+            // ádfasdfasfToolStripMenuItem
+            // 
+            this.ádfasdfasfToolStripMenuItem.Name = "ádfasdfasfToolStripMenuItem";
+            this.ádfasdfasfToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.ádfasdfasfToolStripMenuItem.Text = "ListView";
+            this.ádfasdfasfToolStripMenuItem.Click += new System.EventHandler(this.ádfasdfasfToolStripMenuItem_Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.formatToolStripMenuItem.Text = "format";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.detailToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(105, 26);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(42, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 36);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Detail";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // detailToolStripMenuItem
+            // 
+            this.detailToolStripMenuItem.Name = "detailToolStripMenuItem";
+            this.detailToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.detailToolStripMenuItem.Text = "Detail";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // lstThongTin
+            // 
+            this.lstThongTin.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lstThongTin.FullRowSelect = true;
+            this.lstThongTin.GridLines = true;
+            this.lstThongTin.Location = new System.Drawing.Point(45, 91);
+            this.lstThongTin.MultiSelect = false;
+            this.lstThongTin.Name = "lstThongTin";
+            this.lstThongTin.Size = new System.Drawing.Size(451, 363);
+            this.lstThongTin.TabIndex = 9;
+            this.lstThongTin.UseCompatibleStateImageBehavior = false;
+            this.lstThongTin.View = System.Windows.Forms.View.Details;
+            this.lstThongTin.SelectedIndexChanged += new System.EventHandler(this.lstThongTin_SelectedIndexChanged_2);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Last Name";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "First Name";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Phone";
+            this.columnHeader3.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(878, 481);
+            this.Controls.Add(this.lstThongTin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "ListView Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Button btnAddName;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ádfasdfToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ádfasdfToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ádfasdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ádfasdfasfToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem detailToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ListView lstThongTin;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ BaiTapTuan/BTTuan3/BTTuan3/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTTuan3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddName_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra textbox không trống
+            if (string.IsNullOrWhiteSpace(txtLastName.Text) ||
+                string.IsNullOrWhiteSpace(txtFirstName.Text) ||
+                string.IsNullOrWhiteSpace(txtPhone.Text))
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Tạo 1 dòng mới (ListViewItem)
+            ListViewItem item = new ListViewItem(txtLastName.Text); // Cột 1: Last Name
+            item.SubItems.Add(txtFirstName.Text); // Cột 2: First Name
+            item.SubItems.Add(txtPhone.Text);     // Cột 3: Phone
+
+            // Thêm vào ListView
+            lstThongTin.Items.Add(item);
+
+            // Xóa dữ liệu sau khi thêm
+            txtLastName.Clear();
+            txtFirstName.Clear();
+            txtPhone.Clear();
+            txtLastName.Focus();
+        }
+
+        private void lstThongTin_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ádfasdfasfToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstThongTin_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstThongTin_SelectedIndexChanged_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
